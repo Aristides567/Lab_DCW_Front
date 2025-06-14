ecca217475e802d85794487cd717d560b66e1b0b
Aristides567 <fuenteslfnss@outlook.com>
2025-06-14 03:19:12 -0600

    Update files

--- src/pages/cart/Cart.vue
+++ src/pages/cart/Cart.vue
@@ -91,7 +91,7 @@ const totalFinal = computed(() => +(subtotal.value + impuestos.value).toFixed(2)
               @mouseover="showTechnologiesHover(item.servicioId._id)" @mouseleave="hideTechnologiesHover()">
               <div class="flex flex-col md:flex-row gap-6">
                 <div class="flex-shrink-0">
-                  <img v-if="item.servicioId.imagen" :src="'https://tilinazos.netlify.app/' + item.servicioId.imagen"
+                  <img v-if="item.servicioId.imagen" :src="'https://tilinazos.netlify.app' + item.servicioId.imagen"
                     :alt="item.servicioId.nombre" class="w-32 h-32 object-cover rounded-lg border border-gray-200" />
                 </div>
                 <div class="flex-grow">
